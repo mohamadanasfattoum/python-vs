a3a5d0249720dfbaf520a0b20452f639fc0cbd77
Mohamad anas fattoum <samaanas30@gmail.com>
2023-08-08 17:51:25 +0200

    code1-add-hash()-task

--- code1.py
+++ code1.py
@@ -62,8 +62,22 @@ print(c)
 '''
 
 # hash()
-
+'''
 #the hash() method returns the hash value of an object if it has one. Hash values are just integers that are used to compare dictionary keys during a dictionary look quickly.
 
 a = 'i am anas'
 print (hash(a))
+'''
+def int_hash():
+    n = int(input())
+    result = map(int,input().split())
+    t=tuple(result)
+    print(hash(t))
+
+int_hash()
+
+'''
+if __name__ == '__main__':
+    n = int(input())
+    result = map(int , input().split())
+    print (hash(tuple(result)))'''
